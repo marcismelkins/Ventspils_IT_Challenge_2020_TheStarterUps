4b1961fca76c85c276637d6a3c16a372accbb13a
QQumber <72878357+QQumber@users.noreply.github.com>
2020-10-14 22:23:21 +0300

    Started the file

---
+++ VentspilsIT/Assets/PeopleSpawner.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PeopleSpawner : MonoBehaviour
+{
+    // Reference to the Prefab. Drag a Prefab into this field in the Inspector.
+    public GameObject myPrefab;
+
+    // This script will simply instantiate the Prefab when the game starts.
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Instantiate(myPrefab, new Vector3(10, 0, 0), Quaternion.identity);
+        }
+    }
+}
